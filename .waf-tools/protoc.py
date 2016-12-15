eae24a26c7b40ade1ff7ab356ee8a08bc78dea71
anilj1 <anilj1@umbc.edu>
2016-12-15 10:36:39 -0800

    Fixing protoc invocation issue

--- .waf-tools/protoc.py
+++ .waf-tools/protoc.py
@@ -3,11 +3,14 @@
 # Philipp Bender, 2012
 # Matt Clarkson, 2012
 
+import re
 from waflib.Task import Task
 from waflib.TaskGen import extension 
 
 """
 A simple tool to integrate protocol buffers into your build system.
+
+Example::
 
     def configure(conf):
         conf.load('compiler_cxx cxx protoc')
@@ -19,12 +22,54 @@ A simple tool to integrate protocol buffers into your build system.
                 include  = '. proto',
                 target   = 'executable') 
 
+Notes when using this tool:
+
+- protoc command line parsing is tricky.
+
+  The generated files can be put in subfolders which depend on
+  the order of the include paths.
+
+  Try to be simple when creating task generators
+  containing protoc stuff.
+
 """
 
 class protoc(Task):
+	# protoc expects the input proto file to be an absolute path.
 	run_str = '${PROTOC} ${PROTOC_FLAGS} ${PROTOC_ST:INCPATHS} ${SRC[0].abspath()}'
 	color   = 'BLUE'
 	ext_out = ['.h', 'pb.cc']
+	def scan(self):
+		"""
+		Scan .proto dependencies
+		"""
+		node = self.inputs[0]
+
+		nodes = []
+		names = []
+		seen = []
+
+		if not node: return (nodes, names)
+
+		def parse_node(node):
+			if node in seen:
+				return
+			seen.append(node)
+			code = node.read().splitlines()
+			for line in code:
+				m = re.search(r'^import\s+"(.*)";.*(//)?.*', line)
+				if m:
+					dep = m.groups()[0]
+					for incpath in self.env.INCPATHS:
+						found = incpath.find_resource(dep)
+						if found:
+							nodes.append(found)
+							parse_node(found)
+						else:
+							names.append(dep)
+
+		parse_node(node)
+		return (nodes, names)
 
 @extension('.proto')
 def process_protoc(self, node):
@@ -34,7 +79,8 @@ def process_protoc(self, node):
 	self.source.append(cpp_node)
 
 	if 'cxx' in self.features and not self.env.PROTOC_FLAGS:
-		self.env.PROTOC_FLAGS = '--cpp_out=%s' % node.parent.get_bld().abspath()
+                self.env.PROTOC_FLAGS = ['--cpp_out=%s' % node.parent.get_bld().abspath(),
+                                         '--proto_path=%s' % node.parent.get_src().abspath()]
 
 	use = getattr(self, 'use', '')
 	if not 'PROTOBUF' in use:
@@ -44,4 +90,3 @@ def configure(conf):
 	conf.check_cfg(package="protobuf", uselib_store="PROTOBUF", args=['--cflags', '--libs'])
 	conf.find_program('protoc', var='PROTOC')
 	conf.env.PROTOC_ST = '-I%s'
-
